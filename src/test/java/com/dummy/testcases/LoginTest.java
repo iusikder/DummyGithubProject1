c85271863894c545b37f00e3370c8c15318c2793
iusikder <iusikder@yahoo.com>
2019-04-17 02:03:48 -0400

    Added Message to display

--- src/test/java/com/dummy/testcases/LoginTest.java
+++ src/test/java/com/dummy/testcases/LoginTest.java
@@ -1,5 +1,11 @@
 package com.dummy.testcases;
 
+import org.testng.annotations.Test;
+
 public class LoginTest {
 
+	@Test
+	public void loginTest(){
+		System.out.println("Login Test has started");
+	}
 }
